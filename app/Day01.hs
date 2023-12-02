bef217edf771229a4c5382793a182c8494c80bf4
James Jackson <james@pop-os.localdomain>
2023-12-01 20:01:19 -0600

    Day 1

--- app/Day01.hs
+++ app/Day01.hs
@@ -1,24 +1,37 @@
 module Day01 (day01) where
 import Game.Advent
 import Paths_Advent2023Hs (getDataFileName)
+import Data.Char (isDigit)
+datafile filename = (getDataFileName $ "data/" ++ filename) >>= readFile 
 
-datafile filename = getDataFileName filename >>= readFile
+{- | Part 1.  Input is a list of lines containing letters and numbers.
+Ignoring the letters, take the first and last digit from each line,
+make them into a two-digit number, and add them all up.
 
-{- | Part 1.  Short description of the problem.
->>> d01p1 "day01-ex.txt"
-"TODO"
+>>> d01p1
+"day01-ex.txt" "142"
 -}
 d01p1 filename = do
   input <- datafile filename
-  return "Day 01 Part 1 result"
+  let result  = sum . map (\line -> read (take 1 line) * 10 + read (take 1 (reverse line))) . map (filter isDigit) $ lines input
+  return $ show result
 
-{- | Part 2.  Short description of the problem.
+{- | Part 2. Same as before, but some of the letters are digits spelled
+   out. Now for each line take the first digit or spelled-out digit
+   and the last spelled-out digit, then make two-digit numbers and add
+   them up like in step one.
+
 >>> d01p2 "day01-ex.txt"
-"TODO"
+ "281"
 -}
 d01p2 filename = do
   input <- datafile filename
-  return "Day 01 Part 1 result"
+  return
+    . show
+    . sum
+    . map (\line -> read (firstDigit digits line) * 10 +
+            read (lastDigit digits line))
+    $ lines input
 
 
 day01 :: Day
@@ -26,5 +39,33 @@ day01 =
   Day
     { dayName = "01",
       dayPart1 = d01p1 "day01.txt",
-      dayPart2 = d01p1 "day01.txt"
+      dayPart2 = d01p2 "day01.txt"
     }
+digits :: [(String, String)]
+digits =
+  [("one", "1"),
+    ( "two" ,"2"),
+    ( "three", "3"),
+    ( "four", "4"),
+    ( "five", "5"),
+    ( "six", "6"),
+    ( "seven", "7"),
+    ( "eight", "8"),
+    ( "nine", "9")]
+  
+
+firstDigit :: [(String, String)] -> String -> String
+firstDigit digits string = go digits string
+  where go _ [] = []
+        go [] string = go digits $ tail string
+        go (digit: digits) string =
+          if isDigit (head string) then
+            take 1 string
+          else if take (length (fst digit)) string == fst digit then
+                 snd digit
+               else go digits string
+  
+lastDigit digits string =
+  firstDigit
+     (map (\(k, v) -> (reverse k, v)) digits)
+     (reverse string)
