ae80fbc62743a34d0e33bc5e03b46cc63c72b590
James Jackson <j.e.m.jackson@gmail.com>
2025-05-03 08:05:49 -0500

    Add emacs package for haskell-lsp to nix shell.

--- default.nix
+++ default.nix
@@ -1,11 +1,14 @@
-let pkgs = import <nixpkgs>  {};
-    ePkgs = pkgs.extend(self: super: {
-        advent-runner = self.haskellPackages.callPackage ./advent-runner.nix {};
-    });
-    Advent2023Hs = ePkgs.haskellPackages.developPackage {
-        root = ./.;
-        modifier = (self: 
-                        pkgs.haskell.lib.addBuildDepends 
-                            self [pkgs.cabal-install pkgs.haskellPackages.haskell-language-server]);
-    };
+let
+  pkgs = import <nixpkgs> { };
+  ePkgs = pkgs.extend (self: super: {
+    advent-runner = self.haskellPackages.callPackage ./advent-runner.nix { };
+  });
+  Advent2023Hs = ePkgs.haskellPackages.developPackage {
+    root = ./.;
+    modifier = (self:
+      pkgs.haskell.lib.addBuildTools (pkgs.haskell.lib.addBuildDepends self [
+        pkgs.cabal-install
+        pkgs.haskellPackages.haskell-language-server
+      ]) [ pkgs.emacsPackages.lsp-haskell ]);
+  };
 in Advent2023Hs
